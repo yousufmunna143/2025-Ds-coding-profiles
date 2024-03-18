de05c461d887de654d679844389b3039afe8f3aa
Yousuf shaik <121728204+yousufmunna143@users.noreply.github.com>
2024-03-18 22:09:07 +0530

    Delete coding leaderboard directory

--- coding leaderboard/user.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-st.title("LeaderBoard of Coding Profiles")
-data=pd.read_excel('scores.xlsx')
-df=pd.DataFrame(data) 
-st.dataframe(df, use_container_width=True, hide_index=True)
