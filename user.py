7c72f823cfd584ef28864ba8d03c35ec04b10477
Yousuf shaik <121728204+yousufmunna143@users.noreply.github.com>
2024-03-18 22:29:06 +0530

    Update user.py

--- user.py
+++ user.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.set_page_config(page_title="2025 DS Leader board",
                    layout="wide")
-st.title("LeaderBoard of Coding Profiles")
+st.title("2025 DS Batch LeaderBoard")
 data=pd.read_excel('scores.xlsx')
 df=pd.DataFrame(data) 
 st.dataframe(df, use_container_width=True, hide_index=True)
