a13a0f31fa92d75430960ccbf5e2334d19d7a7d4
Yousuf shaik <121728204+yousufmunna143@users.noreply.github.com>
2024-03-19 12:55:19 +0530

    removed layout wide

--- user.py
+++ user.py
@@ -2,8 +2,7 @@ import streamlit as st
 from openpyxl import load_workbook
 import pandas as pd
 
-st.set_page_config(page_title="2025 DS Leader board",
-                   layout="wide")
+st.set_page_config(page_title="2025 DS Leader board")
 st.title("2025 DS Batch LeaderBoard")
 data=pd.read_excel('scores.xlsx')
 df=pd.DataFrame(data) 
